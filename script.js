68fa3452f54b9a7f2a16254343bcafec686f6633
unknown <Alternativa@ALT-0273.alternativamais.mce>
2024-09-17 18:04:18 -0300

    começo

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleMode(){
+         const Html= document.documentElement
+        Html.classList.toggle('Light')
+        const img= document.querySelector("#profile img")
+        if(Html.classList.contains('Light')){
+
+            img.setAttribute("src","./assets/avatar-light.png")
+        } else{
+            img.setAttribute("src","./assets/avatar.png")
+
+    }
+}
